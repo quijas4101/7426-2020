78dd6d05572b37afce3bfd9b73289ab7382aebf0
quijas4101 <37895150+quijas4101@users.noreply.github.com>
2020-02-19 18:31:35 -0800

    Create MultiLooper.java

---
+++ src/main/java/frc/robot/MultiLooper.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+import java.util.Vector;
+
+/**
+ * @author Team 254
+ *
+ */
+
+public class MultiLooper implements Loopable {
+	
+	private Looper m_looper;
+	private Vector<Loopable> m_loopables = new Vector<Loopable>();
+	
+	public MultiLooper(String name, double period)	{
+		m_looper = new Looper(name, this, period);
+	}
+	
+	@Override
+	public void update() {
+		for(int i = 0; i < m_loopables.size(); i++)	{
+			m_loopables.get(i).update();
+		}
+	}
+	
+	public void addLoopable(Loopable loopable)	{
+		m_loopables.add(loopable);
+	}
+	
+	public void start()	{
+		m_looper.start();
+	}
+	
+	public void stop()	{
+		m_looper.stop();
+	}
+
+}
