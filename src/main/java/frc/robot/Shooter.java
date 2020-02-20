4edae1a68d30a7dafa02f9d7b6c2242726da486a
quijas4101 <37895150+quijas4101@users.noreply.github.com>
2020-02-19 18:26:04 -0800

    Adding BangBang Control

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -8,22 +8,47 @@ import com.revrobotics.CANEncoder.*;
 public class Shooter {
   private static CANSparkMax ShMotor1 = new CANSparkMax(11, MotorType.kBrushless);
   private static CANSparkMax ShMotor2 = new CANSparkMax(10, MotorType.kBrushless);
-
+  private static CANEncoder m_encoder;
+  private static CANEncoder m_encoder2;
   private static SpeedControllerGroup ShooterObj = new SpeedControllerGroup(ShMotor1, ShMotor2);
+  private static long lastTime;
+  private double lastTicks;
 
   public static void Initialize() {
     ShMotor1.setInverted(true);
     ShMotor2.setInverted(false);
     ShMotor2.restoreFactoryDefaults();
     ShMotor1.restoreFactoryDefaults();
+    
+     m_encoder = ShMotor1.getEncoder();
+     //m_encoder2 = ShMotor2.getEncoder();
   }
 
-  public static void SetPower(boolean Enabled, double Power) {
-    if (Enabled) {
-      ShooterObj.set(((-Power)+1)/2);
-    } else {
-      ShooterObj.set(0);
-    }
+  	//Calculate RPM using Java built in timer and Encoder class from WPILIB
+	double getRPM(){
+		double dt = (System.currentTimeMillis() - lastTime);
+		double lastTime = System.currentTimeMillis();
+		double currentTicks = m_encode.getPosition();
+		double rate = (currentTicks - lastTicks)/dt;
+		return (rate * 60 * 1000);	
+	}	
+  
+  public class BangBang implements Loopable {
+	  private double m_desiredRPM = 5800; //Random number, you'll have to test for the RPM that works best for you
+    
+    public static void SetPower(boolean Enabled, double Power) {
+     if (Enabled) {
+      	if(getRPM() < m_desiredRPM){
+			ShooterObj.set(1);
+	  	}
+	  	else{
+			ShooterObj.set(0);
+		   }
+	  	if(Math.abs(getRPM()-m_desiredRPM)<50){
+			///Time to SHOOT
+		  }
+     }
+  
   }
 
   /*//
